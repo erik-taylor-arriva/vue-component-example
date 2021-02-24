249246d6ed0a2c61afed8184783068e27d481685
erik-taylor-arriva <erik.taylor@arrivia.com>
2021-02-24 14:54:29 -0800

    API Key Comment

--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -28,6 +28,9 @@ export default {
     };
   },
   async created() {
+    // You will need to go to https://rapidapi.com/rapidapi/api/movie-database-imdb-alternative
+    // and register for an API key to get this movie example to work correctly.
+    // Simply replace [process.env.VUE_APP_API_KEY] wtih your API key below, or register it in a local .env file
     let apiKey = process.env.VUE_APP_API_KEY;
 
     fetch("https://movie-database-imdb-alternative.p.rapidapi.com/?i=tt0047034&r=json", {
